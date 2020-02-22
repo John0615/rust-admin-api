77912002141a9b1125a51c0ab63704122678a9d9
john.zhao <john.zhao@newType.io>
2020-02-22 20:59:23 +0800

    验证登陆密码

--- src/modules/user/util.rs
+++ src/modules/user/util.rs
@@ -1,7 +1,10 @@
 use super::model::Users;
+use md5::compute;
 
 
 pub fn verify(user: &Users, password: &str) -> bool {
-    true
+    let Users { password_digest,salt, .. } = user;
+    let compute_password = format!("{:x}", compute(format!("{}{}", password, salt)));
+    &compute_password == password_digest
 }
 
