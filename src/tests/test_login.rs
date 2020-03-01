c1f962a256df189bf4239f2bdefe81bb5b6a620c
john.zhao <john.zhao@newType.io>
2020-03-01 22:41:38 +0800

    修改测试用例

--- src/tests/test_login.rs
+++ src/tests/test_login.rs
@@ -1,10 +1,13 @@
 use actix_web::{test, web, App, HttpResponse};
+use crate::graphql;
 
 
 #[actix_rt::test]
 async fn test_index_get() {
-    let mut app = test::init_service(App::new().service(web::resource("/graphql").to(|| async { HttpResponse::Ok() }))).await;
+    let mut app = test::init_service(App::new().configure(graphql::route)).await;
     let req = test::TestRequest::post().uri("/graphql").to_request();
     let resp = test::call_service(&mut app, req).await;
-    assert!(resp.status().is_success());
+    eprintln!("Listening on 0.0.0.0:{:?}", resp.response());
+
+    assert_eq!(resp.status(), 200);
 }
