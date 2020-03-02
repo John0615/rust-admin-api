c3f010eaad3bdc9587d7626d4f9d3d2de10d8f5e
john <john.zhao@newtype.io>
2020-03-02 14:15:03 +0800

    修改测试用例

--- src/tests/test_login.rs
+++ src/tests/test_login.rs
@@ -1,13 +1,26 @@
 use actix_web::{test, web, App, HttpResponse};
 use crate::graphql;
+use crate::database;
+use crate::cli_args;
 
 
 #[actix_rt::test]
 async fn test_index_get() {
-    let mut app = test::init_service(App::new().configure(graphql::route)).await;
+    let opt = {
+      use structopt::StructOpt;
+      cli_args::Opt::from_args()
+    };
+    let schema = std::sync::Arc::new(crate::graphql::schemas::root::create_schema());
+    let mut app = test::init_service(
+      App::new()
+        .data(database::pool::establish_connection(opt.clone()))
+        .data(schema.clone())
+        .data(opt.clone())
+        .configure(graphql::route)
+    ).await;
     let req = test::TestRequest::post().uri("/graphql").to_request();
     let resp = test::call_service(&mut app, req).await;
-    eprintln!("Listening on 0.0.0.0:{:?}", resp.response());
+    eprintln!("resp==result>>>>>>:{:?}", resp.response().error());
 
     assert_eq!(resp.status(), 200);
 }
