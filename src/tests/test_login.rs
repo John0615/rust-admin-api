893673c772fade901cb0b83df924244161a36a8d
john <john.zhao@newtype.io>
2020-03-03 10:09:55 +0800

    修改测试用例

--- src/tests/test_login.rs
+++ src/tests/test_login.rs
@@ -1,8 +1,13 @@
-use actix_web::{test, web, App, HttpResponse};
+use actix_web::{test,App, http::header};
 use crate::graphql;
 use crate::database;
 use crate::cli_args;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize)]
+pub struct Login {
+    data: String
+}
 
 #[actix_rt::test]
 async fn test_index_get() {
@@ -18,9 +23,12 @@ async fn test_index_get() {
         .data(opt.clone())
         .configure(graphql::route)
     ).await;
-    let req = test::TestRequest::post().uri("/graphql").to_request();
-    let resp = test::read_response_json(&mut app, req).await;
-    eprintln!("resp==result>>>>>>:{:?}", resp);
+    let req = test::TestRequest::post()
+          .uri("/graphql")
+          .header(header::CONTENT_TYPE, "application/json")
+          .to_request();
+    let resp: Login = test::read_response_json(&mut app, req).await;
+    eprintln!("resp==result>>>>>>:{:?}", resp.data);
 
     assert_eq!(100, 200);
 }
