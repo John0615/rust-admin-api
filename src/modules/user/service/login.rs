86f66402498f7c91f252ad80ac762550972fee76
john.zhao <john.zhao@newType.io>
2020-05-05 08:51:03 +0800

    生成新token

--- src/modules/user/service/login.rs
+++ src/modules/user/service/login.rs
@@ -47,19 +47,18 @@ pub fn refresh_token(context: &Context) -> ServiceResult<Token> {
         None => Err(ServiceError::Unauthorized),
         Some(ref m) => {
             eprintln!("ksksk{:?}", m);
-            // match create_token(
-            //     &SlimUser {
-            //         login_name: m.login_name,
-            //         email: m.email,
-            //         role: m.role,
-            //     },
-            //     24,
-            // ) {
-            //     Ok(r) => Ok(Token { bearer: Some(r) }),
-            //     Err(e) => Err(e),
-            // }
-            // Ok(m)
-            Ok(Token { bearer: Some(String::from("kkksk")) })
+            match create_token(
+                &SlimUser {
+                    login_name: m.login_name.clone(),
+                    email: m.email.clone(),
+                    role: m.role.clone(),
+                },
+                24,
+            ) {
+                Ok(r) => Ok(Token { bearer: Some(r) }),
+                Err(e) => Err(e),
+            }
+            // Ok(Token { bearer: Some(String::from("kkksk")) })
         },
     }
 }
